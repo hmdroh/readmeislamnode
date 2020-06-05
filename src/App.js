602caeb47c48972c34071fff8a058344123622a2
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2020-06-05 00:39:42 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './readmeislam-ico.svg';
 import './App.css';
 
 function App() {
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Join us, by liking.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/readmeislam"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Read Me Islam Facebook
         </a>
       </header>
     </div>
