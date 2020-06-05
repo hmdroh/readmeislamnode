eb9a2633503c7c1d0b1cf920e3dd09183680787c
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2020-06-04 23:51:28 -0700

    Updates

---
+++ precache-manifest.a2ca8fad1bce4e1d0fc34c2edb7bb955.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782e07b9d49da650c32021514a63bf72",
+    "url": "/readmeislamnode/index.html"
+  },
+  {
+    "revision": "8495f2373f7b3e796134",
+    "url": "/readmeislamnode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "254fc66edd8b8f491075",
+    "url": "/readmeislamnode/static/js/2.4c601c1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/readmeislamnode/static/js/2.4c601c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8495f2373f7b3e796134",
+    "url": "/readmeislamnode/static/js/main.7e8e45d7.chunk.js"
+  },
+  {
+    "revision": "155abcf76395041e3501",
+    "url": "/readmeislamnode/static/js/runtime-main.33dde9fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/readmeislamnode/static/media/logo.5d5d9eef.svg"
+  }
+]);
